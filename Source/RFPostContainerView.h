a6c150f741e2bba8cc55b1cff1a2652051fa1368
Manton Reece <manton@me.com>
2017-10-26 08:57:29 -0500

    Fixed letting clicks fall through below post view.

---
+++ Source/RFPostContainerView.h
@@ -0,0 +1,13 @@
+//
+//  RFPostContainerView.h
+//  Snippets
+//
+//  Created by Manton Reece on 10/26/17.
+//  Copyright © 2017 Riverfold Software. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface RFPostContainerView : NSView
+
+@end
---
+++ Source/RFPostContainerView.m
@@ -0,0 +1,18 @@
+//
+//  RFPostContainerView.m
+//  Snippets
+//
+//  Created by Manton Reece on 10/26/17.
+//  Copyright © 2017 Riverfold Software. All rights reserved.
+//
+
+#import "RFPostContainerView.h"
+
+@implementation RFPostContainerView
+
+- (void) mouseDown:(NSEvent *)event
+{
+	// don't let clicks fall through to timeline underneath
+}
+
+@end
