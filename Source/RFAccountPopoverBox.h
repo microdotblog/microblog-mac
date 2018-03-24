e41e1317b854425b32735fc1a9765ebb61b9c2ad
Manton Reece <manton@me.com>
2018-03-24 11:29:15 -0500

    Mouseover and show accounts.

---
+++ Source/RFAccountPopoverBox.h
@@ -0,0 +1,16 @@
+//
+//  RFAccountPopoverBox.h
+//  Snippets
+//
+//  Created by Manton Reece on 3/24/18.
+//  Copyright © 2018 Riverfold Software. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface RFAccountPopoverBox : NSBox
+
+@property (strong, nonatomic) NSTrackingArea* customTrackingArea;
+@property (strong, nonatomic) NSColor* savedFillColor;
+
+@end
---
+++ Source/RFAccountPopoverBox.m
@@ -0,0 +1,72 @@
+//
+//  RFAccountPopoverBox.m
+//  Snippets
+//
+//  Created by Manton Reece on 3/24/18.
+//  Copyright © 2018 Riverfold Software. All rights reserved.
+//
+
+#import "RFAccountPopoverBox.h"
+
+#import "RFSettings.h"
+#import "RFAccount.h"
+#import "RFConstants.h"
+
+@implementation RFAccountPopoverBox
+
+- (void) awakeFromNib
+{
+	self.savedFillColor = self.fillColor;
+}
+
+- (void) updateTrackingAreas
+{
+	if (self.customTrackingArea) {
+		[self removeTrackingArea:self.customTrackingArea];
+	}
+
+	self.customTrackingArea = [[NSTrackingArea alloc] initWithRect:self.bounds options:NSTrackingActiveInKeyWindow | NSTrackingMouseMoved | NSTrackingMouseEnteredAndExited owner:self userInfo:nil];
+	[self addTrackingArea:self.customTrackingArea];
+}
+
+- (BOOL) hasMultipleAccounts
+{
+	return [RFSettings accounts].count > 1;
+}
+
+- (void) mouseDown:(NSEvent *)event
+{
+	if ([self hasMultipleAccounts]) {
+		NSMenu* menu = [[NSMenu alloc] initWithTitle:@"Accounts"];
+		
+		NSArray* accounts = [RFSettings accounts];
+		for (RFAccount* a in accounts) {
+			NSString* s = [NSString stringWithFormat:@"@%@", a.username];
+			NSMenuItem* item = [[NSMenuItem alloc] initWithTitle:s action:@selector(switchAccount:) keyEquivalent:@""];
+			item.representedObject = a;
+			[menu addItem:item];
+		}
+
+		[NSMenu popUpContextMenu:menu withEvent:event forView:self];
+	}
+}
+
+- (void) switchAccount:(NSMenuItem *)item
+{
+	RFAccount* a = item.representedObject;
+	NSLog (@"switch account: %@", a.username);
+}
+
+- (void) mouseEntered:(NSEvent *)event
+{
+	if ([self hasMultipleAccounts]) {
+		self.fillColor = [NSColor colorWithWhite:0.7 alpha:0.5];
+	}
+}
+
+- (void) mouseExited:(NSEvent *)event
+{
+	self.fillColor = self.savedFillColor;
+}
+
+@end
