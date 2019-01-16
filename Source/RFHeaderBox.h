2ce375acf006ecc676b60a8577e8aea9977c869a
Manton Reece <manton@me.com>
2019-01-16 16:31:29 -0600

    Fixed header background color in dark/light mode.

---
+++ Source/RFHeaderBox.h
@@ -0,0 +1,17 @@
+//
+//  RFHeaderBox.h
+//  Snippets
+//
+//  Created by Manton Reece on 1/16/19.
+//  Copyright © 2019 Riverfold Software. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface RFHeaderBox : NSBox
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Source/RFHeaderBox.m
@@ -0,0 +1,25 @@
+//
+//  RFHeaderBox.m
+//  Snippets
+//
+//  Created by Manton Reece on 1/16/19.
+//  Copyright © 2019 Riverfold Software. All rights reserved.
+//
+
+#import "RFHeaderBox.h"
+
+#import "NSAppearance+Extras.h"
+
+@implementation RFHeaderBox
+
+- (void) awakeFromNib
+{
+	if ([NSAppearance rf_isDarkMode]) {
+		self.fillColor = [NSColor windowBackgroundColor];
+	}
+	else {
+		self.fillColor = [NSColor whiteColor];
+	}
+}
+
+@end
