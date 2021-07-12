3359a1731b670c631bd134d13b9c2008069cdc42
Manton Reece <manton@me.com>
2021-07-12 10:54:50 -0500

    Finished .bar HTML export with MF2.

--- Source/RFBarExportController.m
+++ Source/RFBarExportController.m
@@ -10,6 +10,7 @@
 
 #import "RFPost.h"
 #import "UUDate.h"
+#import "MMMarkdown.h"
 #import <ZipArchive.h>
 
 @implementation RFBarExportController
@@ -28,14 +29,71 @@
 	return post.text;
 }
 
+- (NSString *) placeholderForPost:(RFPost *)post
+{
+	return [NSString stringWithFormat:@"[placeholder_post_%@]", post.postID];
+}
+
 - (NSXMLDocument *) makeHTML
 {
 	NSXMLElement* root = [NSXMLNode elementWithName:@"html"];
 	NSXMLDocument* doc = [[NSXMLDocument alloc] initWithRootElement:root];
-	[doc setVersion:@"1.0"];
+	[doc setDocumentContentKind:NSXMLDocumentHTMLKind];
 	[doc setCharacterEncoding:@"UTF-8"];
 	
-	// ...
+	NSXMLElement* body = [NSXMLNode elementWithName:@"body"];
+	[root addChild:body];
+
+	NSXMLElement* feed_div = [NSXMLNode elementWithName:@"div"];
+	[body addChild:feed_div];
+
+	NSXMLNode* feed_class = [NSXMLNode attributeWithName:@"class" stringValue:@"h-feed"];
+	[feed_div addAttribute:feed_class];
+
+	for (RFPost* post in self.posts) {
+		NSXMLElement* entry_div = [NSXMLNode elementWithName:@"div"];
+		[feed_div addChild:entry_div];
+
+		NSXMLNode* entry_class = [NSXMLNode attributeWithName:@"class" stringValue:@"h-entry"];
+		[entry_div addAttribute:entry_class];
+
+		if (post.title.length > 0) {
+			NSXMLElement* h1_tag = [NSXMLNode elementWithName:@"h1" stringValue:post.title];
+			[entry_div addChild:h1_tag];
+
+			NSXMLNode* h1_class = [NSXMLNode attributeWithName:@"class" stringValue:@"p-name"];
+			[h1_tag addAttribute:h1_class];
+		}
+
+		NSXMLElement* a_tag = [NSXMLNode elementWithName:@"a"];
+		[entry_div addChild:a_tag];
+
+		NSXMLNode* a_href = [NSXMLNode attributeWithName:@"href" stringValue:post.url];
+		[a_tag addAttribute:a_href];
+
+		NSXMLNode* a_class = [NSXMLNode attributeWithName:@"class" stringValue:@"u-url"];
+		[a_tag addAttribute:a_class];
+
+		NSXMLElement* time_tag = [NSXMLNode elementWithName:@"time" stringValue:[post.postedAt description]];
+		[a_tag addChild:time_tag];
+
+		NSXMLNode* time_datetime = [NSXMLNode attributeWithName:@"datetime" stringValue:[post.postedAt uuRfc3339String]];
+		[time_tag addAttribute:time_datetime];
+
+		NSXMLNode* time_class = [NSXMLNode attributeWithName:@"class" stringValue:@"dt-published"];
+		[time_tag addAttribute:time_class];
+
+		NSXMLElement* content_div = [NSXMLNode elementWithName:@"div"];
+		[entry_div addChild:content_div];
+
+		NSXMLNode* content_class = [NSXMLNode attributeWithName:@"class" stringValue:@"e-content"];
+		[content_div addAttribute:content_class];
+		
+		NSString* placeholder = [self placeholderForPost:post];
+		
+		NSXMLNode* content_html = [NSXMLNode textWithStringValue:placeholder];
+		[content_div addChild:content_html];
+	}
 	
 	return doc;
 }
@@ -71,6 +129,13 @@
 
 	NSXMLDocument* doc = [self makeHTML];
 	NSString* html_s = [doc XMLString];
+	
+	for (RFPost* post in self.posts) {
+		NSError* error = nil;
+		NSString* post_html = [MMMarkdown HTMLStringWithMarkdown:post.text error:&error];
+		NSString* post_placeholder = [self placeholderForPost:post];
+		html_s = [html_s stringByReplacingOccurrencesOfString:post_placeholder withString:post_html];
+	}
 
 	NSString* html_path = [self.exportFolder stringByAppendingPathComponent:@"index.html"];
 	[html_s writeToFile:html_path atomically:YES encoding:NSUTF8StringEncoding error:&error];
@@ -83,11 +148,15 @@
 	[json_s writeToFile:json_path atomically:YES encoding:NSUTF8StringEncoding error:&error];
 	
 	NSString* downloads_folder = [self.exportFolder stringByDeletingLastPathComponent];
-	NSString* zip_path = [downloads_folder stringByAppendingPathComponent:@"Blog.bar"];
+	NSString* zip_path = [downloads_folder stringByAppendingPathComponent:@"Micro.blog.bar"];
 	[SSZipArchive createZipFileAtPath:zip_path withContentsOfDirectory:self.exportFolder];
 	
-	// TODO: switch to using temp folder and prompt for final location
-	// ...
+	NSString* new_path = [self promptSave:@"Micro.blog.bar"];
+	if (new_path) {
+		[[NSFileManager defaultManager] copyItemAtPath:zip_path toPath:new_path error:&error];
+	}
+
+	[self cleanupExport];
 }
 
 @end
