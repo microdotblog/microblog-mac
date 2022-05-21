bd030b8d3c8f8b3a02563412c5f46f8c9e2d623f
Manton Reece <manton@me.com>
2022-05-21 11:37:02 -0500

    Fixed drawing and selection problems in books table.

---
+++ Source/MBBooksTableView.h
@@ -0,0 +1,17 @@
+//
+//  MBBooksTableView.h
+//  Micro.blog
+//
+//  Created by Manton Reece on 11/10/21.
+//  Copyright © 2021 Micro.blog. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MBBooksTableView : NSTableView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Source/MBBooksTableView.m
@@ -0,0 +1,41 @@
+//
+//  MBBooksTableView.m
+//  Micro.blog
+//
+//  Created by Manton Reece on 11/10/21.
+//  Copyright © 2021 Micro.blog. All rights reserved.
+//
+
+#import "MBBooksTableView.h"
+
+#import "RFAllPostsController.h"
+
+@implementation MBBooksTableView
+
+- (void) drawContextMenuHighlightForRow:(NSInteger)row
+{
+	// override to avoid the focus highlight rectangle
+}
+	
+- (void) keyDown:(NSEvent *)event
+{
+	if ([[event characters] isEqualToString:@"\r"]) {
+		if ([self.delegate respondsToSelector:@selector(openRow:)]) {
+			[self.delegate performSelector:@selector(openRow:) withObject:nil];
+		}
+	}
+	else {
+		[super keyDown:event];
+	}
+}
+
+- (void) willOpenMenu:(NSMenu *)menu withEvent:(NSEvent *)event
+{
+	NSInteger row = [self clickedRow];
+	if (row >= 0) {
+		NSIndexSet* index_set = [NSIndexSet indexSetWithIndex:row];
+		[self selectRowIndexes:index_set byExtendingSelection:NO];
+	}
+}
+
+@end
