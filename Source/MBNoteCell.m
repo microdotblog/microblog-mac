bd5d6b2a2f2e5b5b0afd70bc61608f084cbcd3cf
Manton Reece <manton@me.com>
2023-12-12 13:38:35 -0600

    Various notes table and cell fixes.

--- Source/MBNoteCell.m
+++ Source/MBNoteCell.m
@@ -14,7 +14,39 @@
 
 - (void) setupWithNote:(MBNote *)note
 {
-	[self.textView setStringValue:note.text];
+	NSString* s = note.text;
+	if (s.length > 100) {
+		s = [s substringToIndex:100];
+		s = [s stringByAppendingString:@"..."];
+	}
+	
+	[self.textView setStringValue:s];
+}
+
+- (void) drawBackgroundInRect:(NSRect)dirtyRect
+{
+	CGRect r = self.bounds;
+	[self.backgroundColor set];
+	NSRectFill (r);
+}
+
+- (void) drawSelectionInRect:(NSRect)dirtyRect
+{
+	CGRect r = self.bounds;
+	if ([self.superview isKindOfClass:[NSTableView class]]) {
+		NSTableView* table = (NSTableView *)self.superview;
+		if (![table.window isKeyWindow]) {
+			[[NSColor colorNamed:@"color_row_unfocused_selection"] set];
+		}
+		else if (table.window.firstResponder == table) {
+			[[NSColor selectedContentBackgroundColor] set];
+		}
+		else {
+			[[NSColor colorNamed:@"color_row_unfocused_selection"] set];
+		}
+	}
+	
+	NSRectFill (r);
 }
 
 @end
