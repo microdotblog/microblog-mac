d911869dd60ab8c38336fd412179b27e9c73ebab
Manton Reece <manton@me.com>
2023-12-12 10:35:37 -0600

    Added basic notes key window.

---
+++ Source/MBNotesKeyController.h
@@ -0,0 +1,17 @@
+//
+//  MBNotesKeyController.h
+//  Micro.blog
+//
+//  Created by Manton Reece on 12/11/23.
+//  Copyright © 2023 Micro.blog. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MBNotesKeyController : NSWindowController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Source/MBNotesKeyController.m
@@ -0,0 +1,18 @@
+//
+//  MBNotesKeyController.m
+//  Micro.blog
+//
+//  Created by Manton Reece on 12/11/23.
+//  Copyright © 2023 Micro.blog. All rights reserved.
+//
+
+#import "MBNotesKeyController.h"
+
+@implementation MBNotesKeyController
+
+- (void) windowDidLoad
+{
+	[super windowDidLoad];
+}
+
+@end
