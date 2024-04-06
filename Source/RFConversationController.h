8eb614829a5305d83bc9c59449dd09d959d71a92
Manton Reece <manton@me.com>
2024-04-06 18:38:45 -0500

    Fixed new selection for conversation.

--- Source/RFConversationController.h
+++ Source/RFConversationController.h
@@ -8,8 +8,9 @@
 
 #import <Cocoa/Cocoa.h>
 #import <WebKit/WebKit.h>
+#import "MBSimpleTimelineController.h"
 
-@interface RFConversationController : NSViewController
+@interface RFConversationController : MBSimpleTimelineController
 
 @property (strong, nonatomic) IBOutlet NSTextField* headerField;
 @property (strong, nonatomic) IBOutlet WebView* webView;
