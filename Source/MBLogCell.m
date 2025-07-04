971d0a5dd8752b048f32c49ffb8d728786b696ea
Manton Reece <manton@me.com>
2025-07-04 11:01:45 -0500

    Added green text for publish log lines.

--- Source/MBLogCell.m
+++ Source/MBLogCell.m
@@ -10,6 +10,8 @@
 
 #import "MBLog.h"
 
+static NSString* const kPublishLogPhrase = @"Publish: Done";
+
 @implementation MBLogCell
 
 - (void) setupWithLog:(MBLog *)log
@@ -22,6 +24,42 @@
 
 	self.dateField.stringValue = [formatter stringFromDate:log.date];
 	self.messageField.stringValue = log.message;
+
+	if ([log.message containsString:kPublishLogPhrase]) {
+		// reset as green text
+		self.dateField.attributedStringValue = [self greenStringForText:self.dateField.stringValue];
+		self.messageField.attributedStringValue = [self greenStringForText:self.messageField.stringValue];
+	}
+}
+
+- (void) setSelected:(BOOL)selected
+{
+	[super setSelected:selected];
+	
+	// when selected, reset the color if needed
+	if ([self.messageField.stringValue containsString:kPublishLogPhrase]) {
+		self.dateField.attributedStringValue = [self greenStringForText:self.dateField.stringValue];
+		self.messageField.attributedStringValue = [self greenStringForText:self.messageField.stringValue];
+	}
+}
+
+- (NSAttributedString *) greenStringForText:(NSString *)text
+{
+	NSColor* color;
+	if (self.isSelected) {
+		color = [NSColor colorNamed:@"color_logs_publish_selected"];
+	}
+	else {
+		color = [NSColor colorNamed:@"color_logs_publish"];
+	}
+	
+	NSFont* bold_font = [NSFont boldSystemFontOfSize:self.messageField.font.pointSize];
+	NSMutableAttributedString* attr = [[NSMutableAttributedString alloc] initWithString:text];
+
+	[attr addAttribute:NSForegroundColorAttributeName value:color range:NSMakeRange(0, attr.length)];
+	[attr addAttribute:NSFontAttributeName value:bold_font range:NSMakeRange(0, attr.length)];
+	
+	return attr;
 }
 
 @end
