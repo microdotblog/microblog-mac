0991f9a0dd1f3ccfdf6e3aac36f1a55f27d950eb
Manton Reece <manton@me.com>
2025-11-30 19:53:32 -0600

    Fixed video playback bar by adding minimum size, update title.

--- Source/MBVideoZoomController.m
+++ Source/MBVideoZoomController.m
@@ -8,6 +8,8 @@
 
 #import "MBVideoZoomController.h"
 
+#import "RFConstants.h"
+
 @implementation MBVideoZoomController
 
 - (id) initWithURL:(NSString *)videoURL
@@ -24,11 +26,33 @@
 {
 	[super windowDidLoad];
 	
+	[self setupVideo];
+	[self updateTitle];
+	
+	self.window.delegate = self;
+}
+
+- (void) setupVideo
+{
 	NSURL* url = [NSURL URLWithString:self.videoURL];
 	if (url) {
 		NSURLRequest* request = [NSURLRequest requestWithURL:url];
 		[self.webView loadRequest:request];
 	}
+}
+
+- (void) updateTitle
+{
+	NSURL* url = [NSURL URLWithString:self.videoURL];
+	if (url) {
+		self.window.title = url.host;
+	}
+}
+
+- (BOOL) windowShouldClose:(NSWindow *)sender
+{
+	[[NSNotificationCenter defaultCenter] postNotificationName:kPhotoWindowDidCloseNotification object:self];
+	return YES;
 }
 
 - (void) downloadPhoto
