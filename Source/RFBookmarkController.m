0ebb44b6820465379a1f4ce8e026a76f82b21c84
Manton Reece <manton@me.com>
2026-06-08 10:34:14 -0700

    Started adding client-side bookmark summaries with AI if available.

--- Source/RFBookmarkController.m
+++ Source/RFBookmarkController.m
@@ -8,9 +8,20 @@
 
 #import "RFBookmarkController.h"
 
+#import "MBRobotsModel.h"
 #import "RFClient.h"
 #import "RFConstants.h"
 #import "RFMacros.h"
+#import "RFSettings.h"
+#import "Micro_blog-Swift.h"
+
+static NSString* const kBookmarkSummaryPrompt = @"Summarize the following text in one short sentence. Do not start the summary with the text \"The author\" or \"The writer\" or \"The speaker\".\n\n%@";
+
+@interface RFBookmarkController ()
+
+@property (assign, nonatomic) BOOL isSaving;
+
+@end
 
 @implementation RFBookmarkController
 
@@ -57,18 +68,90 @@
 
 - (IBAction) saveBookmark:(id)sender
 {
+	if (self.isSaving) {
+		return;
+	}
+
+	self.isSaving = YES;
 	[self.progressSpinner startAnimation:nil];
-	
+
 	NSString* url = self.urlField.stringValue;
 
+	[self downloadHTMLForURLString:url completion:^(NSString* html) {
+		[self summaryForBookmarkHTML:html urlString:url completion:^(NSString* summary) {
+			[self saveBookmarkWithURLString:url summary:summary];
+		}];
+	}];
+}
+
+- (void) downloadHTMLForURLString:(NSString *)urlString completion:(void (^)(NSString* html))completion
+{
+	NSURL* url = [NSURL URLWithString:urlString];
+	if (url == nil) {
+		completion(@"");
+		return;
+	}
+
+	NSURLRequest* request = [NSURLRequest requestWithURL:url cachePolicy:NSURLRequestReloadIgnoringLocalCacheData timeoutInterval:30.0];
+	NSURLSessionDataTask* task = [[NSURLSession sharedSession] dataTaskWithRequest:request completionHandler:^(NSData* data, NSURLResponse* response, NSError* error) {
+		if (error || (data.length == 0)) {
+			NSLog(@"Error downloading bookmark HTML %@: %@", urlString, error);
+			RFDispatchMainAsync (^{
+				completion(@"");
+			});
+			return;
+		}
+
+		NSString* html = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+		if (html == nil) {
+			html = [[NSString alloc] initWithData:data encoding:NSISOLatin1StringEncoding];
+		}
+
+		RFDispatchMainAsync (^{
+			completion(html ?: @"");
+		});
+	}];
+	[task resume];
+}
+
+- (void) summaryForBookmarkHTML:(NSString *)html urlString:(NSString *)urlString completion:(void (^)(NSString* summary))completion
+{
+	if ((html.length == 0) || ![RFSettings boolForKey:kIsUsingAI] || ![MBRobotsModel isLocalModelAvailable]) {
+		completion(@"");
+		return;
+	}
+
+	[MBBookmarkReadability textContentFromHTML:html baseURLString:urlString completion:^(NSString* text) {
+		if (text.length == 0) {
+			completion(@"");
+			return;
+		}
+
+		NSString* prompt = [NSString stringWithFormat:kBookmarkSummaryPrompt, text];
+		[MBRobotsModel runPrompt:prompt completion:^(NSString* result) {
+			NSString* summary = [result stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
+			completion(summary ?: @"");
+		}];
+	}];
+}
+
+- (void) saveBookmarkWithURLString:(NSString *)urlString summary:(NSString *)summary
+{
 	RFClient* client = [[RFClient alloc] initWithPath:@"/micropub"];
-	NSDictionary* args = @{
+	NSMutableDictionary* args = [@{
 		@"h": @"entry",
 		@"content": @"",
-		@"bookmark-of": url
-	};
+		@"bookmark-of": urlString
+	} mutableCopy];
+
+	if (summary.length > 0) {
+		args[@"summary"] = summary;
+	}
+
 	[client postWithParams:args completion:^(UUHttpResponse* response) {
 		RFDispatchMainAsync (^{
+			self.isSaving = NO;
+			[self.progressSpinner stopAnimation:nil];
 			[self.window performClose:nil];
 		});
 	}];
